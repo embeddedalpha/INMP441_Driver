589c72eb51d20c24c9c0182746f83bb7abcd97a5
Kunal Salvi <kunalsalvi63@gmail.com>
2024-06-15 23:42:39 -0500

    Updated I2S.c

--- I2S/I2S.c
+++ I2S/I2S.c
@@ -15,57 +15,62 @@
  *
  */
 
+DMA_Config I2S2_DMA_FD;
+DMA_Config I2S3_DMA_FD;
+DMA_Config I2S2_DMA_HD;
+DMA_Config I2S3_DMA_HD;
+
 static int8_t SCK_PIN_INIT2(I2S_Config *config)
 {
-	if(config->Full_Duplex.SCK_Pin == I2S_SCK_Pin.I2S2.PB10){
+	if(config->Full_Duplex.SCK_Pin == I2S_Pin.SCK.I2S2.PB10){
 		GPIO_Pin_Init(GPIOB, 10, MODE.Alternate_Function, Output_Type.Push_Pull, Speed.Very_High_Speed, Pull.No_Pull_Up_Down, Alternate_Functions.I2S_2EXT);
 	}
-	else if(config->Full_Duplex.SCK_Pin == I2S_SCK_Pin.I2S2.PB13){
+	else if(config->Full_Duplex.SCK_Pin == I2S_Pin.SCK.I2S2.PB13){
 		GPIO_Pin_Init(GPIOB, 13, MODE.Alternate_Function, Output_Type.Push_Pull, Speed.Very_High_Speed, Pull.No_Pull_Up_Down, Alternate_Functions.I2S_2EXT);
 	}
 	else{
-		config->Error.SCK_Pin_Error = 1;
+		config->Error.SCK_Pin_Error = true;
 		return -1;
 	}
 	return 1;
 }
 static int8_t WS_PIN_INIT2(I2S_Config *config)
 {
-	if(config->Full_Duplex.WS_Pin == I2S_WS_Pin.I2S2.PB09){
+	if(config->Full_Duplex.WS_Pin == I2S_Pin.WS.I2S2.PB09){
 		GPIO_Pin_Init(GPIOB, 9, MODE.Alternate_Function, Output_Type.Push_Pull, Speed.Very_High_Speed, Pull.No_Pull_Up_Down, Alternate_Functions.I2S_2EXT);
 	}
-	else if(config->Full_Duplex.WS_Pin == I2S_WS_Pin.I2S2.PB12){
+	else if(config->Full_Duplex.WS_Pin == I2S_Pin.WS.I2S2.PB12){
 		GPIO_Pin_Init(GPIOB, 12, MODE.Alternate_Function, Output_Type.Push_Pull, Speed.Very_High_Speed, Pull.No_Pull_Up_Down, Alternate_Functions.I2S_2EXT);
 	}
 	else{
-		config->Error.WS_Pin_Error = 1;
+		config->Error.WS_Pin_Error = true;
 		return -1;
 	}
 	return 1;
 }
 static int8_t SD_PIN_Init2(I2S_Config *config)
 {
-	if(config->Full_Duplex.SD_Pin == I2S_SD_Pin.I2S2.PB15){
+	if(config->Full_Duplex.SD_Pin == I2S_Pin.SD.I2S2.PB15){
 		GPIO_Pin_Init(GPIOB, 15, MODE.Alternate_Function, Output_Type.Push_Pull, Speed.Very_High_Speed, Pull.No_Pull_Up_Down, Alternate_Functions.I2S_2EXT);
 	}
-	else if(config->Full_Duplex.SD_Pin == I2S_SD_Pin.I2S2.PC03){
+	else if(config->Full_Duplex.SD_Pin == I2S_Pin.SD.I2S2.PC03){
 		GPIO_Pin_Init(GPIOC, 3, MODE.Alternate_Function, Output_Type.Push_Pull, Speed.Very_High_Speed, Pull.No_Pull_Up_Down, Alternate_Functions.I2S_2EXT);
 	}
 	else{
-		config->Error.SD_Pin_Error = 1;
+		config->Error.SD_Pin_Error = true;
 		return -1;
 	}
 	return 1;
 }
 static int8_t MCK_PIN_Init2(I2S_Config *config)
 {
-	if(config->Full_Duplex.MCK_Pin != I2S_MCK_Pin.I2S2.Disable)
+	if(config->Full_Duplex.MCK_Pin != I2S_Pin.MCK.I2S2.Disable)
 	{
-		if((config->Full_Duplex.MCK_Pin == I2S_MCK_Pin.I2S2.PC7)){
+		if((config->Full_Duplex.MCK_Pin == I2S_Pin.MCK.I2S2.PC7)){
 			GPIO_Pin_Init(GPIOC, 7, MODE.Alternate_Function, Output_Type.Push_Pull, Speed.Very_High_Speed, Pull.No_Pull_Up_Down, Alternate_Functions.I2S_2);
 		}
 		else{
-			config->Error.MCK_PIN_Error = 1;
+			config->Error.MCK_PIN_Error = true;
 			return -1;
 		}
 	}
@@ -73,14 +78,14 @@ static int8_t MCK_PIN_Init2(I2S_Config *config)
 }
 static int8_t EXT_SD_PIN_Init2(I2S_Config *config)
 {
-	if(config->Full_Duplex.EXT_SD == I2S_ext_SD_Pin.I2S2.PB14){
+	if(config->Full_Duplex.EXT_SD == I2S_Pin.EXT_SD.I2S2.PB14){
 		GPIO_Pin_Init(GPIOB, 14, MODE.Alternate_Function, Output_Type.Push_Pull, Speed.Very_High_Speed, Pull.No_Pull_Up_Down, Alternate_Functions.I2S_2EXT);
 	}
-	else if(config->Full_Duplex.EXT_SD == I2S_ext_SD_Pin.I2S2.PC02){
+	else if(config->Full_Duplex.EXT_SD == I2S_Pin.EXT_SD.I2S2.PC02){
 		GPIO_Pin_Init(GPIOC, 2, MODE.Alternate_Function, Output_Type.Push_Pull, Speed.Very_High_Speed, Pull.No_Pull_Up_Down, Alternate_Functions.I2S_2EXT);
 	}
 	else{
-		config->Error.Ext_SD_Pin_Error = 1;
+		config->Error.Ext_SD_Pin_Error = true;
 		return -1;
 	}
 	return 1;
@@ -101,15 +106,24 @@ int8_t I2S_Init(I2S_Config *config)
 		if(config->Full_Duplex.Enable == true){
 			EXT_SD_PIN_Init2(config);
 		}
+		else if(config->Half_Duplex.Enable == true)
+		{
+
+		}
 
 
 
 	}else if(config->Port == I2S_Port.I2S3)
 	{
+		RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;
 
 	}else{
 
+		config->Error.Port_Error = true;
+
 	}
+
+
 
 	config->Port->I2SCFGR |= 1 << 11;
 
@@ -126,7 +140,7 @@ int8_t I2S_Init(I2S_Config *config)
 	}
 
 	// Standard
-	if(config->Standard == I2S_Standard.I2S_Philips){
+	if(config->Standard == I2S_Standard.Standard_Philips){
 		config->Port->I2SCFGR &= ~(3 << 4);
 	}else if(config->Standard == I2S_Standard.Left_Justified){
 		config->Port->I2SCFGR |= (1 << 4);
@@ -135,7 +149,7 @@ int8_t I2S_Init(I2S_Config *config)
 	}else if(config->Standard == I2S_Standard.PCM){
 		config->Port->I2SCFGR |= (3 << 4);
 	}else{
-		config->Error.Standard_Error = 1;
+		config->Error.Standard_Error  = true;
 	}
 
 	if(config->Data_Length == I2S_Data_Length._16_bit){
@@ -145,7 +159,7 @@ int8_t I2S_Init(I2S_Config *config)
 	}else if(config->Data_Length == I2S_Data_Length._32_bit){
 		config->Port->I2SCFGR |= (2<1);
 	}else{
-		config->Error.Data_Len_Error = 1;
+		config->Error.Data_Len_Error = true;
 	}
 
 
@@ -154,18 +168,126 @@ int8_t I2S_Init(I2S_Config *config)
 	}else if(config->Channel_Length == I2S_Channel_Length._16_bit){
 		config->Port->I2SCFGR =  (1<<0);
 	}else{
-		config->Error.Channel_Length_Error = 1;
+		config->Error.Channel_Length_Error = true;
+	}
+
+
+	int plli2s_n = 128;
+	int plli2s_r = 5;
+	if(config->Audio_Frequency == I2S_Audio_Frequency._8000KHz)
+	{
+		//
+		  plli2s_n = 64;
+		  plli2s_r = 5;
+		RCC -> PLLI2SCFGR = (plli2s_n << 6) | (plli2s_r << 28);
+		RCC -> CR |= RCC_CR_PLLI2SON;
+		while(!(RCC -> CR & RCC_CR_PLLI2SRDY));
+		config->Port->I2SPR |= 50;
+		//
+	}else if(config->Audio_Frequency == I2S_Audio_Frequency._16000Hz)
+	{
+		//
+		  plli2s_n = 128;
+		  plli2s_r = 5;
+		RCC -> PLLI2SCFGR = (plli2s_n << 6) | (plli2s_r << 28);
+		RCC -> CR |= RCC_CR_PLLI2SON;
+		while(!(RCC -> CR & RCC_CR_PLLI2SRDY));
+		config->Port->I2SPR |= 50;
+		//
+	}else if(config->Audio_Frequency == I2S_Audio_Frequency._22050Hz)
+	{
+		//
+	}else if(config->Audio_Frequency == I2S_Audio_Frequency._32000Hz)
+	{
+		//
+		  plli2s_n = 64;
+		  plli2s_r = 5;
+		RCC -> PLLI2SCFGR = (plli2s_n << 6) | (plli2s_r << 28);
+		RCC -> CR |= RCC_CR_PLLI2SON;
+		while(!(RCC -> CR & RCC_CR_PLLI2SRDY));
+		config->Port->I2SPR |= 12;
+		//
+	}else if(config->Audio_Frequency == I2S_Audio_Frequency._44100Hz)
+	{
+		//
+	}else if(config->Audio_Frequency == I2S_Audio_Frequency._48000Hz)
+	{
+		//
+		  plli2s_n = 96;
+		  plli2s_r = 5;
+		RCC -> PLLI2SCFGR = (plli2s_n << 6) | (plli2s_r << 28);
+		RCC -> CR |= RCC_CR_PLLI2SON;
+		while(!(RCC -> CR & RCC_CR_PLLI2SRDY));
+		config->Port->I2SPR |= 12;
+		//
+	}else if(config->Audio_Frequency == I2S_Audio_Frequency._64000Hz)
+	{
+		//
+		  plli2s_n = 128;
+		  plli2s_r = 5;
+		RCC -> PLLI2SCFGR = (plli2s_n << 6) | (plli2s_r << 28);
+		RCC -> CR |= RCC_CR_PLLI2SON;
+		while(!(RCC -> CR & RCC_CR_PLLI2SRDY));
+		config->Port->I2SPR |= 12;
+		//
+	}else if(config->Audio_Frequency == I2S_Audio_Frequency._96000Hz)
+	{
+		//
+		  plli2s_n = 192;
+		  plli2s_r = 5;
+		RCC -> PLLI2SCFGR = (plli2s_n << 6) | (plli2s_r << 28);
+		RCC -> CR |= RCC_CR_PLLI2SON;
+		while(!(RCC -> CR & RCC_CR_PLLI2SRDY));
+		config->Port->I2SPR |= 12;
+		//
+	}else if(config->Audio_Frequency == I2S_Audio_Frequency._192000Hz)
+	{
+		//
+	}else if(config->Audio_Frequency == I2S_Audio_Frequency._384000Hz)
+	{
+		//
+	}else
+	{
+		config->Error.Audio_Frequency_Error = true;
 	}
 
 	//  I2S Enable
 	config->Port->I2SCFGR |= 1 << 10;
 
-
-
-
-
-
-
 	return 1;
+
+}
+
+
+void I2S_Print_Errors(I2S_Config *config)
+{
+	if(config->Error.Audio_Frequency_Error) printConsole("Audio_Frequency_Error \r\n");
+	if(config->Error.Channel_Length_Error) printConsole("Channel_Length_Error \r\n");
+	if(config->Error.Data_Len_Error) printConsole("Data_Len_Error \r\n");
+	if(config->Error.Ext_SD_Pin_Error) printConsole("Ext_SD_Pin_Error \r\n");
+	if(config->Error.MCK_PIN_Error) printConsole("MCK_PIN_Error \r\n");
+	if(config->Error.Mode_Error) printConsole("Mode_Error \r\n");
+	if(config->Error.Port_Error) printConsole("Port_Error \r\n");
+	if(config->Error.SCK_Pin_Error) printConsole("SCK_Pin_Error \r\n");
+	if(config->Error.SD_Pin_Error) printConsole("SD_Pin_Error \r\n");
+	if(config->Error.Standard_Error) printConsole("Standard_Error \r\n");
+	if(config->Error.WS_Pin_Error) printConsole("WS_Pin_Error \r\n");
+}
+
+void I2S_Read_Left_Data(I2S_Config *config, void *data, uint16_t len)
+{
+	if(config->Port == I2S_Port.I2S2)
+	{
+		if(config->Full_Duplex.Enable == ENABLE)
+		{
+			I2S2_DMA_FD.buffer_length = len;
+			I2S2_DMA_FD.memory_address = (uint32_t)&data;
+			I2S2_DMA_FD.peripheral_address = (config->Port->DR);
+
+
+
+		}
+	}
+
 
 }
